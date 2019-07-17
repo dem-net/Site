0f032cd498c89a09c6956d1ab1386d7e0a090cec
Xavier Fischer <xfischer@cityway.fr>
2019-07-17 14:42:33 +0200

    Update GpxUpload.vue

--- dem-net-vue/src/components/GpxUpload.vue
+++ dem-net-vue/src/components/GpxUpload.vue
@@ -21,7 +21,7 @@ import axios from 'axios'
 
     export default {
         name: 'GpxUpload',
-        components: { axios },
+        components: {  },
         data() {
             return {
                 file: null
