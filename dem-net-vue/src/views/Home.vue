32c2f59bb016a5e9ca4476947a427ccf26608a51
xfischer <xavier@fischer.ninja>
2020-04-20 00:08:35 +0200

    news

--- dem-net-vue/src/views/Home.vue
+++ dem-net-vue/src/views/Home.vue
@@ -41,6 +41,8 @@
           </div> 
         </div>
         <b-message title="News" type="is-info" size="is-small" icon-pack="fas" has-icon>
+            Apr 19 2020: - SketchFab export ! <img src="../assets/sketchfab-logo-color.png"/>
+            <br/>
             Mar 10 2020: - Attributions and Thunderforest imagery
             <br/>
             Mar 07 2020: - OSM models / NASADEM dataset / search available in map
